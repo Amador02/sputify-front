1ab677c3a66d24e1055dfaaecbe649755ffd6dc2
Juan Manuel Amador Roa <115110279+Amador02@users.noreply.github.com>
2024-04-23 09:08:14 -0500

    Create a search text field

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,62 +1,18 @@
-import '../assets/styles/search.css';
-import React, { useState } from 'react';
 import transicion from './transicion';
+import SearchTextField from './SearchTextfield';
 
-const SearchComp = ({ onSearch }) => {
-    const [searchTerm, setSearchTerm] = useState('');
 
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onSearch(searchTerm);
-    };
-
-    return (
-        <div className="flex flex-row relative justify-center items-center w-fit h-fit bg-white rounded-lg">
-            <button className="relative left-2 p-1">
-                <svg
-                    width="17"
-                    height="16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    className="w-5 h-5 text-black"
-                >
-                    <path
-                        d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
-                        stroke="currentColor"
-                        strokeWidth="1.333"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    ></path>
-                </svg>
-            </button>
-            <input type="text"
-                className="bg-white outline-none border border-transparent text-neutral-900 placeholder-neutral-400 text-sm rounded-lg focus:ring-white  focus:border-transparent block w-64 p-2.5 checked:bg-emerald-500"
-                placeholder="Buscar..." />
-            <button className="relative mr-2">
-                <svg
-                    className="w-5 h-5 text-gray-700"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                    ></path>
-                </svg>
-            </button>
-        </div>
-
-    );
-};
 const Search = () => {
-    return transicion(SearchComp);
+    return transicion(
+        newFunction
+    );
 }
 
 export default Search;
+
+function newFunction() {
+    return (<div className='m-1 flex flex-col gap-1'>
+        <label>Busqueda: </label>
+        <SearchTextField />
+    </div>);
+}
---
+++ src/components/SearchTextfield.jsx
@@ -0,0 +1,58 @@
+import '../assets/styles/search.css';
+import React, { useState } from 'react';
+
+const SearchComp = ({ onSearch = (text) => { console.log(text) } }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(searchTerm);
+    };
+    const clearText = (e) => {
+        e.preventDefault();
+        setSearchTerm('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="flex flex-row relative justify-center items-center w-fit h-fit bg-white rounded-lg">
+                <button type='submit' className="relative left-2 p-1">
+                    <svg
+                        width="17"
+                        height="16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        role="img"
+                        className="w-5 h-5 text-black"
+                    >
+                        <path
+                            d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+                            stroke="currentColor"
+                            strokeWidth="1.333"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>
+                    </svg>
+                </button>
+                <input required={{}} id='search' type='text' value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }}
+                    className="bg-white outline-none border border-transparent text-neutral-900 placeholder-neutral-400 text-sm rounded-lg focus:ring-white  focus:border-transparent block w-64 p-2.5 checked:bg-emerald-500"
+                    placeholder="Buscar..." />
+                <button type='button' className="relative mr-2" onClick={clearText}>
+                    <svg
+                        className="w-5 h-5 text-gray-700"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                        ></path>
+                    </svg>
+                </button>
+            </div>
+        </form>
+    );
+};
+export default SearchComp;
